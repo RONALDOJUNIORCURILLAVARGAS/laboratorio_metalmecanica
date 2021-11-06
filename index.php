45e6927b720665d789051bba335a2dd88808eccf
RONALDO JUNIOR CURILLA VARGAS <74572974+RONALDOJUNIORCURILLAVARGAS@users.noreply.github.com>
2021-11-06 12:20:53 -0500

    conexion de index a login

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   Hola mundo prueba 1
-</body>
-</html>
+<?php
+//include_once './settings/user_session.php';
+//$userSession=new UserSession();
+session_start();
+
+if(isset($_SESSION['estado']) )
+{
+   header('location:./views/principal.php');
+}
+else{
+  //require_once './views/acceso.php';  
+  header('location:./views/login.php');
+}
+
+?>
+
