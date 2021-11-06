41a2271fc012f5ed63bf03923a3ea96c7f00ca5b
RONALDO JUNIOR CURILLA VARGAS <74572974+RONALDOJUNIORCURILLAVARGAS@users.noreply.github.com>
2021-11-06 12:13:14 -0500

    creacion de conexion de index y login

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//include_once './settings/user_session.php';
+//$userSession=new UserSession();
+session_start();
+
+
+if(isset($_SESSION['estado']) )
+{
+   header('location:./views/principal.php');
+}
+else{
+  //require_once './views/acceso.php';  
+  header('location:./views/login.php');
+}
+
+?>
