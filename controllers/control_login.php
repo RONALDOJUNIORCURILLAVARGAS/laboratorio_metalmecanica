618c4393a94ae8fbb122005c70a5264ebf023289
RONALDO JUNIOR CURILLA VARGAS <74572974+RONALDOJUNIORCURILLAVARGAS@users.noreply.github.com>
2021-11-06 21:23:36 -0500

    creacion de conexionbd,controllers,bean,dao de usuarios

---
+++ controllers/control_login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require_once '../models/DAO/usuariosDAO.php';
+require_once '../models/BEAN/usuariosBEAN.php';
+$objususarioBEAN = new usuariosBEAN();
+$objususarioDAO  = new usuariosDAO();
+$correo=$_POST['correo'];
+$pass=$_POST['clave'];
+$objususarioBEAN ->setCORREO($correo);
+$objususarioBEAN ->setPASS($pass);
+$lista=$objususarioDAO->IngresarUsuario($objususarioBEAN); 
+    if($lista==null)
+            {
+            /*Asignamos el estado a la sesion estado*/
+            $_SESSION['estado']=false;   
+            header('location:../views/login.php');
+            break;
+    }
+    else {
+            $_SESSION['estado']=true;
+            header('location:../views/principal.php');
+            break;
+    }
+?>
---
+++ models/DAO/usuariosDAO.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../utils/conexionBD.php'; 
+require_once '../BEAN/usuariosBEAN.php'; 
+class usuariosDAO {
+
+    public function IngresarUsuario(usuariosBEAN $objPersonaBean) {
+        try{
+            $sql="SELECT * FROM USUARIO WHERE CORREO_USUARIO='$objPersonaBean->CORREO'  AND  PASSWORD_USUARIO='$objPersonaBean->PASS' ; ";
+            $objc=new conexionBD();
+            $cn=$objc->getConexionBD();
+            $rs=mysqli_query($cn,$sql);
+            $lista=array();
+            if($rs){       
+                while($fila=mysqli_fetch_assoc($rs)){
+                    $lista[]=$fila;
+                } 
+            }
+            else{
+                $lista=null;
+            }
+            mysqli_close($cn);   
+        }
+        catch(Exception $exc){
+        }
+        return $lista;
+    }
+
+
+}
+?>
