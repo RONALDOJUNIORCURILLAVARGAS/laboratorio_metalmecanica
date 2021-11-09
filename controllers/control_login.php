cb4e2127b9d3183e30d0bcfe723a4e1c3eeb4aaa
RONALDO JUNIOR CURILLA VARGAS <74572974+RONALDOJUNIORCURILLAVARGAS@users.noreply.github.com>
2021-11-09 11:34:56 -0500

    prueba

--- controllers/control_login.php
+++ controllers/control_login.php
@@ -9,26 +9,20 @@ $pass=$_POST['clave'];
 $objususarioBEAN ->setCORREO($correo);
 $objususarioBEAN ->setPASS($pass);
 $lista=$objususarioDAO->IngresarUsuario($objususarioBEAN); 
+$validar=0;
     if($lista==null)
             {
             /*Asignamos el estado a la sesion estado*/
+            
             $_SESSION['estado']=false;   
             header('location:../views/login.php');
-<<<<<<< HEAD
-            break;
+            $validar=1;
     }
-    else {
-            $_SESSION['estado']=true;
-            header('location:../views/principal.php');
-            break;
-    }
-=======
-        
-    }
-    
-            $_SESSION['estado']=true;
-            header('location:../views/principal.php');
        
+    if($validar==0){
+            //Cambiar a true
+            $_SESSION['estado']=false;
+            header('location:../views/principal.php');
+       }
     
->>>>>>> 1d84fa74443cd5ebeb8f73bcbe45a8d1805f73b6
 ?>
